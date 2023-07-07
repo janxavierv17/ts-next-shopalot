fb188bc901a167838ea679b4456c2027c08c8457
Jan Virgen <janxavierv17@hotmail.com>
2023-07-07 14:29:33 +1000

    fix: fix GET method paramter types

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,8 +1,7 @@
 import prisma from "@/prisma/client";
 import { NextResponse } from "next/server";
-import { NextApiRequest, NextApiResponse } from "next";
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(req: Request) {
 	try {
 		const users = await prisma.user.findMany();
 		return NextResponse.json({ users });
