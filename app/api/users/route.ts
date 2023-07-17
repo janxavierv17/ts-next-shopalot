553a4a33c0aef97ab0170164a03d64c2c509f5b6
Jan Virgen <janxavierv17@hotmail.com>
2023-07-17 14:13:33 +1000

    feat: initial pin working configuration

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,9 +1,12 @@
 import prisma from "@/prisma/client";
 import { NextResponse } from "next/server";
+import { logger } from "@/app/_utils/logger";
 
 export async function GET(req: Request) {
 	try {
 		const users = await prisma.user.findMany();
 		return NextResponse.json({ users });
-	} catch (err) {}
+	} catch (err) {
+		logger.error(`Error => ${err}`);
+	}
 }
